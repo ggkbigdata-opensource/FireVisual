adf15f0f8fa88fb4d1edacaabee5135d726eedd7
csk <csk8975@126.com>
2017-03-28 14:30:24 +0800

    add page templates and static files

--- src/main/java/com/fire/app/controller/TestController.java
+++ src/main/java/com/fire/app/controller/TestController.java
@@ -1,62 +1,21 @@
 package com.fire.app.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.alibaba.fastjson.JSONObject;
-import com.fire.app.domain.AppFireEventRepository;
-import com.fire.app.domain.AppPunishment;
-import com.fire.app.domain.AppPunishmentRepository;
-import com.fire.app.domain.BsBuildingInfoRepository;
-import com.fire.app.domain.BsEstateManagerRepository;
-import com.fire.app.domain.BsFireSystemRepository;
-import com.fire.app.domain.BsKeyPartRepository;
-import com.fire.app.domain.BsMainFunctionRepository;
 
 /**
  * @fileName TestController.java
  * @author csk
- * @createTime 2017年3月28日 下午12:30:21
+ * @createTime 2017年3月28日 下午2:25:32
  * @version 1.0
  * @function
  */
-@RestController
+@Controller
 public class TestController {
-    
-    @Autowired
-    private AppFireEventRepository fireEventRepo;
-    
-    @Autowired
-    private AppPunishmentRepository punishmentRepo;
-    
-    @Autowired
-    private BsBuildingInfoRepository buidlingInfoRepo;
-    
-    @Autowired
-    private BsEstateManagerRepository estateManagerRepo;
-    
-    @Autowired
-    private BsFireSystemRepository fireSystemRepo;
-    
-    @Autowired
-    private BsKeyPartRepository keyPartRepo;
-    
-    @Autowired
-    private BsMainFunctionRepository mainFunctionRepo;
-    
-    @RequestMapping(value={"testEntity"},method = RequestMethod.GET)
-    public JSONObject TestEntity(){
-        JSONObject result = new JSONObject();
-        result.put("fireEvents", fireEventRepo.findAll());
-        result.put("punishments", punishmentRepo.findAll());
-        result.put("buidlingInfo", buidlingInfoRepo.findAll());
-        result.put("estateManager", estateManagerRepo.findAll());
-        result.put("fireSystem", fireSystemRepo.findAll());
-        result.put("keyPart", keyPartRepo.findAll());
-        return result;
+
+    @RequestMapping(value="alarmFire")
+    public String TestPage(){
+        
+        return "alarm/alarm-fire";
     }
-    
 }
